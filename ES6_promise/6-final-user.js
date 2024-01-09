326b447dc4b3a36f8efb8f85ba9cd7b4d5befc3c
Travis Adamson <tndadamson@gmail.com>
2024-01-09 11:56:16 -0600

    handling multiple promises at once

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const usersData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const filesData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([usersData, filesData]);
+}
